66f655d1f08fee22a32d4cad5e53f134c71df9a4
Roger Thomas <rogerthomas96@outlook.com>
2023-05-04 21:35:17 -0500

    Camera adjustments, bug fixes, enemy introduction
    - Adjusted the camera to follow the player and change zoom depending on the players movement state.
    -Fixed a bug where the player would fall through the world randomly but most commonly after a high jump.
    -Fixed a bug where the player would get stuck on vertical surfaces when jumping and would be able to then jump again.
    -Introduced the enemy character, set up animations for idle/walk, set up left and right movement. Also set up a turning mechanic for when the enemy reaches the end of an area and should turn around.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,27 +11,42 @@ public class PlayerMovement : MonoBehaviour
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
     Animator myAnimator;
+    CapsuleCollider2D myBodyCollider;
+    BoxCollider2D myFeetCollider;
+
+    bool isAlive = true;
     
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        myBodyCollider = GetComponent<CapsuleCollider2D>();
+        myFeetCollider = GetComponent<BoxCollider2D>();
     }
 
     void Update()
     {
+        if(!isAlive) {return;}
         Run();
         FlipSprite();
+        Die();
     }
 
     void OnMove(InputValue value)
     {
+        if(!isAlive) {return;}
         moveInput = value.Get<Vector2>();
         Debug.Log(moveInput);
     }
     
     void OnJump(InputValue value)
     {
+        if(!isAlive) {return;}
+        if(!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
+
         if(value.isPressed)
         {
             myRigidbody.velocity += new Vector2 (0f, jumpSpeed);
@@ -56,6 +71,15 @@ public class PlayerMovement : MonoBehaviour
         transform.localScale = new Vector2 (Mathf.Sign(myRigidbody.velocity.x), 1f);
         }
 
+    }
+
+    void Die()
+    {
+        if(myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemies")))
+        {
+            isAlive = false;
+            myAnimator.SetTrigger("Dying");
+        }
     }
 
 }
